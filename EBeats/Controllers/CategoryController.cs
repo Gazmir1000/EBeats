6c1948741c080fd93c50cf37598d60a77a6493c4
Gazmir1000 <gazmir1000@gmail.com>
2023-05-25 21:29:34 +0200

    fixes

--- EBeats/Controllers/CategoryController.cs
+++ EBeats/Controllers/CategoryController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EBeats.Controllers
 {
     public class CategoryController : Controller
     {
+        
+        [Authorize(Roles ="Admin")]
         public IActionResult Index()
         {
             return View();
